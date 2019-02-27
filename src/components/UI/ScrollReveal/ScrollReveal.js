f2822db784f19428bae3f7fb2f5e30d9f29defad
Test <thompson.doan@gmail.com>
2019-02-27 15:43:05 +1100

    added ScrollReveal, which wil translateY and opacity 1 to component children when it is scrolled into viewport.

---
+++ src/components/UI/ScrollReveal/ScrollReveal.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Transition } from "react-transition-group";
+import PropTypes from "prop-types";
+
+class ScrollAnimation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.setIsReveal = this.setIsReveal.bind(this);
+    this.scrollEventHandler = this.scrollEventHandler.bind(this);
+    this.scrollTail = this.scrollTail.bind(this);
+
+    this.ref = React.createRef();
+
+    this.throttleInterval = 20;
+    this.lastScrollTime = 0;
+
+    this.state = {
+      isReveal: false
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("scroll", this.scrollEventHandler);
+  }
+
+  componentWillUnmount() {
+    this.removeScrollListener();
+  }
+
+  scrollEventHandler() {
+    const now = Date.now();
+    // if condition for throttling purposes
+    if (this.lastScrollTime + this.throttleInterval < now) {
+      if (this.getIsReveal()) {
+        setTimeout(() => {
+          this.setIsReveal(true);
+          this.removeScrollListener();
+        }, this.props.delay);
+      } else {
+        clearTimeout(this.isSetTimeout);
+        this.isSetTimeout = setTimeout(this.scrollTail, this.throttleInterval);
+        this.lastScrollTime = now;
+      }
+    }
+  }
+
+  scrollTail() {
+    console.log("scrollTail");
+    if (!this.state.isReveal && this.getIsReveal()) {
+      setTimeout(() => {
+        this.setIsReveal(true);
+        this.removeScrollListener();
+      }, this.props.delay);
+    }
+  }
+
+  removeScrollListener() {
+    document.removeEventListener("scroll", this.scrollEventHandler);
+    console.log("Scroll event listener removed");
+  }
+
+  setIsReveal = isReveal => {
+    this.setState({ isReveal: isReveal });
+  };
+
+  getIsReveal() {
+    const revealPosY = window.innerHeight * this.props.revealThreshold;
+    return this.ref.current.getBoundingClientRect().top < revealPosY;
+  }
+
+  render() {
+    const defaultStyle = {
+      opacity: 0
+    };
+
+    const transitionStyles = {
+      entering: {
+        opacity: 0,
+        transform: "translateY(50px)"
+      },
+      entered: {
+        opacity: 1,
+        transform: "translateY(0px)",
+        transition: `opacity ${this.props.duration}ms, transform ${
+          this.props.duration
+        }ms cubic-bezier(0,.79,0,-0.18)`
+      }
+    };
+
+    return (
+      <Transition in={this.state.isReveal} timeout={this.props.duration}>
+        {transitionState => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[transitionState]
+            }}
+          >
+            {this.props.children(this.ref)}
+          </div>
+        )}
+      </Transition>
+    );
+  }
+}
+
+ScrollAnimation.propTypes = {
+  duration: PropTypes.number,
+  revealThreshold: PropTypes.number,
+  delay: PropTypes.number
+};
+
+ScrollAnimation.defaultProps = {
+  duration: 400, // duration in milliseconds
+  revealThreshold: 0.95, // reveal position relative to top of viewport. 1 = 100% viewport height.
+  delay: 0
+};
+
+export default ScrollAnimation;
