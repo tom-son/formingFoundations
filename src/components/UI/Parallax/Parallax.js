e679eee4205338b6ca96ba05a1f86f4a33f32c90
Test <thompson.doan@gmail.com>
2019-02-27 15:49:42 +1100

    added Parallax, when component is in viewport translate3d of Y for background to give a parallax effect relative to props.children.

---
+++ src/components/UI/Parallax/Parallax.js
@@ -0,0 +1,67 @@
+import React from "react";
+import classes from "./Parallax.module.scss";
+
+class Parallax extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.backgroundScrollHandler = this.backgroundScrollHandler.bind(this);
+    this.scrollAnimationFrame = this.scrollAnimationFrame.bind(this);
+
+    this.backgroundRef = React.createRef();
+    this.containerRef = React.createRef();
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.scrollAnimationFrame);
+    window.addEventListener("resize", this.scrollAnimationFrame);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(this.scrollAnimationFrame)
+  }
+
+  scrollAnimationFrame() {
+    window.requestAnimationFrame(this.backgroundScrollHandler);
+  }
+
+  backgroundScrollHandler() {
+    const { scrollY, innerHeight } = window;
+    const inViewBottom = scrollY + innerHeight;
+    const containerRect = this.containerRef.current;
+    // console.log(inViewBottom, containerRect.offsetTop);
+    // console.log(scrollY, containerRect.offsetHeight);
+    if (
+      inViewBottom > containerRect.offsetTop &&
+      scrollY < containerRect.offsetTop + containerRect.offsetHeight
+    ) {
+      const backgroundRect = this.backgroundRef.current;
+      // console.log(
+      //   "In viewport:",
+      //   (inViewBottom - containerRect.offsetTop) * 0.15 - (backgroundRect.offsetHeight / 2)
+      // );
+      // console.log(((inViewBottom - containerRect.offsetTop) * 0.15)/ 4)
+      this.backgroundRef.current.style.transform = `translate3d(0px, ${(inViewBottom -
+        containerRect.offsetTop) *
+        0.15 -
+        (backgroundRect.offsetHeight / 5)}px, 0px)`;
+    }
+  }
+
+  render() {
+    const { props } = this;
+    return (
+      <div ref={this.containerRef} className={classes.parallax}>
+        <img
+          ref={this.backgroundRef}
+          className={classes.background}
+          src={props.src}
+          alt="background"
+        />
+        <div className={classes.foreground}>{props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default Parallax;
